dee7ca1ac160e4c07b6090c62648452dbb198449
ViceRabbit <vicerabbit16789@gmail.com>
2024-01-08 01:47:51 +0000

    "hi there"

--- rabbitwebsite/rabbitpolls/models.py
+++ rabbitwebsite/rabbitpolls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class bunnyquestion(models.Model):
+    questiontext = models.CharField(max_length=500)
+    dateofquest = models.DateTimeField("date published rabbit")
+
+    def __str__(self):
+        return self.questiontext
+    
+    def hasrabbit(self):
+        return "rabbit" in self.questiontext
+
+
+class bunnychoice(models.Model):
+    question = models.ForeignKey(bunnyquestion, on_delete=models.CASCADE) # basically all choices are related to one bunnyquestion instance
+    choicetext = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choicetext
+
