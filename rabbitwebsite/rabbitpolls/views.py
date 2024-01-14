682c5802fc04360e51210cf009f3fb5adea12596
ViceRabbit <vicerabbit16789@gmail.com>
2024-01-14 01:04:46 +0000

    htmling lol

--- rabbitwebsite/rabbitpolls/views.py
+++ rabbitwebsite/rabbitpolls/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import bunnyquestion # databasseee
 
 def index(request):
-    return HttpResponse("yo wsg chat; this is the rabbit polls!!")
+    latestquestions = bunnyquestion.objects.order_by("-dateofquest")[:5]
+    context = { # in the html template, we reference stupid variables so
+        "latestquestions": latestquestions,
+    }
+    return render(request, "rabbitpolls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"ok so ur looking at question {question_id}")
+    question = get_object_or_404(bunnyquestion, pk=question_id)
+    return render(request, "rabbitpolls/details.html", {"thequestion": question})
 
 def results(request, question_id):
     return HttpResponse(f"ok so you're looking at the RESULTS of question {question_id}")
